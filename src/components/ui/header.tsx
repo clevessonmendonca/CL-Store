9cb4817e965046c4903b68fec18ee731d64a3ea3
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-10-18 21:08:36 -0300

    feat: add header menu using sheet component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,65 @@
 import React from "react";
 import { Card } from "./card";
 import { Button } from "./button";
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentCircleIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 export const Header = () => {
   return (
     <Card className="flex justify-between items-center p-[1.875rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            <div className="mt-2 flex flex-col gap-2">
+              <Button
+                variant="outline"
+                className="w-full gap-2 flex justify-start "
+              >
+                <LogInIcon size={16} />
+                Fazer Login
+              </Button>
+
+              <Button
+                variant="outline"
+                className="w-full gap-2 flex justify-start "
+              >
+                <HomeIcon size={16} />
+                Início
+              </Button>
+
+              <Button
+                variant="outline"
+                className="w-full gap-2 flex justify-start "
+              >
+                <PercentIcon size={16} />
+                Ofertas
+              </Button>
+
+              <Button
+                variant="outline"
+                className="w-full gap-2 flex justify-start "
+              >
+                <ListOrderedIcon size={16} />
+                Catálogo
+              </Button>
+            </div>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="font-semibold text-lg">
         <span className="text-primary">CL</span> Store
