ffe14356931e649c16610b8e62fcdcda9b3eeb7f
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-10-22 07:55:22 -0300

    feat: add Foooter Component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="bg-accent opacity-75 px-8 py-4 text-[0.625rem]">
+      @ 2023 Copyright <span className="font-semibold">CL Store</span>
+    </div>
+  );
+};
