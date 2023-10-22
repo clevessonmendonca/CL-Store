cb37626d9a2550320c5c4f41356bc989e249a5ec
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-10-22 07:57:55 -0300

    fix: ensure products prices are wrapped in the line

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -10,7 +10,7 @@ interface ProductItemProps {
 
 export const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex flex-col gap-4 max-w-[156px]">
+    <div className="flex flex-col gap-4 max-w-[170px]">
       <div className="relative bg-accent rounded-lg h-[170px] w-[150px] flex items-center justify-center">
         <Image
           src={product.imageUrls[0]}
@@ -32,18 +32,18 @@ export const ProductItem = ({ product }: ProductItemProps) => {
       </div>
 
       <div className="flex flex-col gap-1">
-        <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis ">
+        <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
           {product.name}
         </p>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 ">
           {product.discountPercentage > 0 ? (
             <>
-              <p className="font-semibold">
+              <p className="font-semibold overflow-hidden whitespace-nowrap text-ellipsis">
                 R$ {product.totalPrice.toFixed(2)}
               </p>
 
-              <p className="opacity-75 line-through text-xs">
+              <p className="opacity-75 line-through text-xs overflow-hidden whitespace-nowrap text-ellipsis">
                 R$ {Number(product.basePrice).toFixed(2)}
               </p>
             </>
