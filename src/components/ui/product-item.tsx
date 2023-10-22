6f5049a00d78b95fd4bcc9374942756c84e1a30f
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-10-22 07:23:11 -0300

    feat: add ProductItem and ProductList component

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,15 +1,17 @@
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import React from "react";
+import { Badge } from "./badge";
+import { ArrowDownIcon } from "lucide-react";
 
 interface ProductItemProps {
-  product: Product;
+  product: ProductWithTotalPrice;
 }
 
 export const ProductItem = ({ product }: ProductItemProps) => {
   return (
     <div className="flex flex-col gap-4 max-w-[156px]">
-      <div className="bg-accent rounded-lg h-[170px] w-[150px] flex items-center justify-center">
+      <div className="relative bg-accent rounded-lg h-[170px] w-[150px] flex items-center justify-center">
         <Image
           src={product.imageUrls[0]}
           width={0}
@@ -21,12 +23,36 @@ export const ProductItem = ({ product }: ProductItemProps) => {
           }}
           alt={product.name}
         />
+
+        {product.discountPercentage > 0 && (
+          <Badge className="absolute left-2 top-4 px-2 py-[2px]">
+            <ArrowDownIcon size={12} /> {product.discountPercentage}%
+          </Badge>
+        )}
       </div>
 
-      <div>
+      <div className="flex flex-col gap-1">
         <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis ">
           {product.name}
         </p>
+
+        <div className="flex items-center gap-2">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
+              </p>
+
+              <p className="opacity-75 line-through text-xs">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            </>
+          ) : (
+            <p className="font-semibold text-sm">
+              R$ {product.basePrice.toFixed(2)}
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
