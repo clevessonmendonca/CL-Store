e3af2699e735d231c9e4ef21a68d3fc0e933dbec
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-10-29 11:59:18 -0300

    feat: add stripe checkout stripe

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,10 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     DATABASE_URL: string;
+    HOST_URL: string;
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
+    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
+    STRIPE_SECRET_KEY: string;
   }
 }
--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -7,9 +7,21 @@ import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
+import { createCheckout } from "@/actions/checkout";
+import { loadStripe } from "@stripe/stripe-js";
 
 export const Cart = () => {
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
+
+  const handleFinishProductClick = async () => {
+    const checkout = await createCheckout(products);
+
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    stripe?.redirectToCheckout({
+      sessionId: checkout.id,
+    });
+  };
 
   return (
     <div className="flex flex-col gap-8 h-full">
@@ -69,7 +81,12 @@ export const Cart = () => {
           <p>R$ {total.toFixed(2)}</p>
         </div>
 
-        <Button className="uppercase font-bold mt-7">Finalizar Compra</Button>
+        <Button
+          className="uppercase font-bold mt-7"
+          onClick={handleFinishProductClick}
+        >
+          Finalizar Compra
+        </Button>
       </div>
     </div>
   );
