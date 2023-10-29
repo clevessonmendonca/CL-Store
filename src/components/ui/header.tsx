b8d0e7b4ae02f17fcf98af9878b1238de2f6b2af
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-10-29 14:44:39 -0300

    fix: cart component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -130,7 +130,7 @@ export const Header = () => {
           </Button>
         </SheetTrigger>
 
-        <SheetContent>
+        <SheetContent className="w-[350px]">
           <Cart />
         </SheetContent>
       </Sheet>
